b3f4293e6d3b6663ae4ccea277f1df4186f3b32d
PiyushSaini04 <piyushsaini0404@gma.com>
2025-07-16 14:50:44 +0530

    email

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -140,7 +140,7 @@ const Footer = () => {
 
         <FooterSection theme={theme}>
           <h3>Contact Us</h3>
-          <p>Email: optimustechclub@gmail.com</p>
+          <p>optimus.orgz@gmail.com</p>
           <SocialLinks theme={theme}>
             <a href="https://www.instagram.com/optimus.orgz/" target="_blank" rel="noopener noreferrer">
               <FontAwesomeIcon icon={faInstagram} />
