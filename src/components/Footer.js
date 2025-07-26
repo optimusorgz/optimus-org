896c71e68632e1e4c485a0ee38acd32f51035bd0
OPTIMUS <optimus.orgz@gmail.com>
2025-07-26 23:57:08 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -141,7 +141,7 @@ const Footer = () => {
         <FooterSection theme={theme}>
           <h3>Contact Us</h3>
           <p>optimus.orgz@gmail.com</p>
-          <h3>Social</h3>
+          <h3>Social Media</h3>
           <SocialLinks theme={theme}>
             <a href="https://www.instagram.com/optimus.orgz/" target="_blank" rel="noopener noreferrer">
               <FontAwesomeIcon icon={faInstagram} />
